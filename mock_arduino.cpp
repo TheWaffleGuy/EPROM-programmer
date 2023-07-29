9f8f19ee0297181f48e2f1daa9ca869f05e1aea8
Carl Wallberg <carl.wallberg@gmail.com>
2023-07-30 00:20:47 +0200

    Add more implementations of functions for mock Serial

--- mock_arduino.cpp
+++ mock_arduino.cpp
@@ -143,6 +143,7 @@ class FakeSerial {
 public:
   void begin(unsigned long) {}
   void end() {}
+
   size_t write(char c) {
     std::cout << c;
     return 1;
@@ -151,59 +152,169 @@ public:
     std::cout << std::string((char*)buf,size);
     return size;
   }
-  size_t println(void)
-  {
-    return write("\r\n");
-  }
+
   size_t write(const char *str) {
     if (str == NULL) return 0;
     return write((const uint8_t *)str, strlen(str));
   }
+
+  size_t write(const char *buffer, size_t size) {
+    return write((const uint8_t *)buffer, size);
+  }
+
+  size_t print(const String &s)
+  {
+    return write(s.c_str(), s.length());
+  }
+
   size_t print(const char str[])
   {
     return write(str);
   }
+
+  size_t print(char c)
+  {
+    return write(c);
+  }
+
+  size_t print(unsigned char b, int base)
+  {
+    return print((unsigned long) b, base);
+  }
+
+  size_t print(int n, int base)
+  {
+    return print((long) n, base);
+  }
+
+  size_t print(unsigned int n, int base)
+  {
+    return print((unsigned long) n, base);
+  }
+
+  size_t print(long n, int base)
+  {
+    if (base == 0) {
+      return write(n);
+    } else if (base == 10) {
+      if (n < 0) {
+        int t = print('-');
+        n = -n;
+        return printNumber(n, 10) + t;
+      }
+      return printNumber(n, 10);
+    } else {
+      return printNumber(n, base);
+    }
+  }
+
+  size_t print(unsigned long n, int base)
+  {
+    if (base == 0) return write(n);
+    else return printNumber(n, base);
+  }
+
+  size_t println(void)
+  {
+    return write("\r\n");
+  }
+
+  size_t println(const String &s)
+  {
+    size_t n = print(s);
+    n += println();
+    return n;
+  }
+
   size_t println(const char c[])
   {
     size_t n = print(c);
     n += println();
     return n;
   }
-  int16_t printf(const char *format, ...)
-  {
-    va_list ap;
-    va_start(ap, format);
-    return vprintf(format, ap);
-  }
-  size_t print(char c)
-  {
-    return write(c);
-  }
+
   size_t println(char c)
   {
     size_t n = print(c);
     n += println();
     return n;
   }
+
+  size_t println(unsigned char b, int base)
+  {
+    size_t n = print(b, base);
+    n += println();
+    return n;
+  }
+
+  size_t println(int num, int base)
+  {
+    size_t n = print(num, base);
+    n += println();
+    return n;
+  }
+
+  size_t println(unsigned int num, int base)
+  {
+    size_t n = print(num, base);
+    n += println();
+    return n;
+  }
+
+  size_t println(long num, int base)
+  {
+    size_t n = print(num, base);
+    n += println();
+    return n;
+  }
+
+  size_t println(unsigned long num, int base)
+  {
+    size_t n = print(num, base);
+    n += println();
+    return n;
+  }
+
+  int16_t printf(const char *format, ...)
+  {
+    va_list ap;
+    va_start(ap, format);
+    return vprintf(format, ap);
+  }
+
   int available()
   {
     return true;
   }
+
   String readStringUntil(char terminator)
   {
     using namespace std;
     String ret;
-    /*
-    char c = getch();
-    while (c >= 0 && c != terminator)
-    {
-        ret += (char)c;
-        c = getch();
-    }
+    std::getline (cin, ret, terminator);
     return ret;
-    */
-   std::getline (cin, ret, terminator);
-   return ret;
+  }
+
+
+private:
+size_t printNumber(unsigned long n, uint8_t base)
+  {
+    char buf[8 * sizeof(long) + 1]; // Assumes 8-bit chars plus zero byte.
+    char *str = &buf[sizeof(buf) - 1];
+
+    *str = '\0';
+
+    // prevent crash if called with base == 1
+    if (base < 2) base = 10;
+
+    do {
+      char c = n % base;
+      n /= base;
+
+      *--str = c < 10 ? c + '0' : c + 'A' - 10;
+    } while(n);
+
+    return write(str);
   }
 };
 
